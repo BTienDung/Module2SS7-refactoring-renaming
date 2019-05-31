0e4181080a5a8e46bcb57c65d908a20249963109
BTienDung <tiendung0899@gmail.com>
2019-05-31 08:55:19 +0700

    nb

---
+++ CalcularTest.java
@@ -0,0 +1,11 @@
+package season_7.Rename;
+
+public class CalcularTest {
+    public static void main(String[] args) {
+        int firt = 10;
+        int second = 20;
+        char operator = '+';
+        System.out.println(Calculator.calculate(firt,second,operator));
+    }
+
+}
